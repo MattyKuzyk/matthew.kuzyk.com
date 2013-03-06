47dd8f0352d82302a20064cb631f4fe546f180c0
Matthew Kuzyk <matthew.kuzyk@gmail.com>
2013-03-06 18:47:29 -0500

    Bio randomly changes, awesome

--- site.js
+++ site.js
@@ -1,8 +1,25 @@
+var bio = new Array();
+var randomnumber=Math.floor(Math.random()*4);
+bio[0] = "Blorange Enthusiast";
+bio[1] = "Sloths, mang";
+bio[2] = "Jack of all trades, master of some";
+bio[3] = "What's Gucci?"
+
+
 $(document).ready(function(){
+
+	$('#bio').text(bio[randomnumber]);
 
 	setTimeout(function(){
 
           $('#name').fadeIn(1000);
       },500);
+
+	setTimeout(function(){
+
+          $('#bio').fadeIn(1000);
+      },1000);
+
+
 
       });
