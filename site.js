2fb0318c1f04bb8984180e4ab21618bafb38dc8a
unknown <kuzykm@WSIN795.pointclickcare.local>
2013-03-07 13:21:02 -0500

    yoloswag

--- site.js
+++ site.js
@@ -1,11 +1,14 @@
 var bio = new Array();
-var randomnumber=Math.floor(Math.random()*6);
+var randomnumber=Math.floor(Math.random()*9);
 bio[0] = "Blorange Enthusiast";
 bio[1] = "Sloths, mang";
 bio[2] = "Jack of all trades, master of some";
 bio[3] = "What's Gucci?"
 bio[4] = "I cry Based tears";
 bio[5] = 'div id="yolo" because #yolo';
+bio[6] = 'Not random, just psuedo-random';
+bio[7] = "Is this how I use Tumblr?";
+bio[8] = "Girls say I can't commit, they should my git log";
 
 
 $(document).ready(function(){
