1eb163cb388a4c60a229288fe1bb2b447f46b55b
MattyKuzyk <matthew.kuzyk@gmail.com>
2013-03-18 10:04:11 -0400

    Removed a tagline, cleaned up the code and changed the sloth to a jpg

--- site.js
+++ site.js
@@ -1,14 +1,17 @@
 var bio = new Array();
-var randomnumber=Math.floor(Math.random()*5);
-bio[0] = "Blorange Enthusiast";
-bio[1] = "Jack of all trades, master of some";
-bio[2] = 'div id="yolo" because #yolo';
-bio[3] = 'Not random, just psuedo-random';
-bio[4] = "Girls say I can't commit, they should see my git log";
-/*bio[5] = "What's Gucci?"
+var randomnumber=Math.floor(Math.random()*4);
+
+bio[0] = "Jack of all trades, master of some";
+bio[1] = 'div id="yolo" because #yolo';
+bio[2] = 'Not random, just psuedo-random';
+bio[3] = "Girls say I can't commit, they should see my git log";
+/*
+bio[4] = "Blorange Enthusiast";
+bio[5] = "What's Gucci?"
 bio[6] = "I cry Based tears";
 bio[7] = "Sloths, mang";
-bio[8] = "Is this how I use Tumblr?";*/
+bio[8] = "Is this how I use Tumblr?";
+*/
 
 
 $(document).ready(function(){
